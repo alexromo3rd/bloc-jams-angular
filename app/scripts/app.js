dffff344c3974e0cb225e4d357bfc2031e438a0a
Alex Romo III <alexromo3rd@gmail.com>
2016-11-02 20:49:47 -0700

    Checkpoint 5 complete

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,15 @@
-(function() {
+(function () {
   function config($stateProvider, $locationProvider) {
     $locationProvider
       .html5Mode({
       enabled: true,
       requireBase: false
     });
-    
+
     $stateProvider
       .state('landing', {
         url: '/',
+        controller: 'LandingCtrl as landing',
         templateUrl: '/templates/landing.html'
     })
       .state('album', {
@@ -17,12 +18,12 @@
     })
       .state('collection', {
         url: '/collection',
+        controller: 'CollectionCtrl as collection',
         templateUrl: '/templates/collection.html'
     });
   }
-  
+
   angular
   .module('blocJams', ['ui.router'])
   .config(config);
 })();
-
