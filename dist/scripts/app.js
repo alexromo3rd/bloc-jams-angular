dbefc9ea5ee6427afed7fb2e3bb9db930709c351
Alex Romo III <alexromo3rd@gmail.com>
2016-11-05 14:49:41 -0700

    Assignment 5 complete

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -14,6 +14,7 @@
     })
       .state('album', {
         url: '/album',
+        controller: 'AlbumCtrl as albumPage',
         templateUrl: '/templates/album.html'
     })
       .state('collection', {
